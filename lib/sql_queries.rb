93e7e2c57b2195d6eedbddd112baf45326be55eb
Trac Nguyen <nguyendtrac@gmail.com>
2018-06-06 16:36:58 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -25,9 +25,9 @@ def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
-"SELECT projects.title, SUM(pledges.amount) - projects.funding_goal AS amount_over
+"SELECT project.title, SUM(pledges.amount) - projects.funding_goal AS amount_over
  FROM projects
- INNER JOIN pledges
+ JOIN pledges
  ON projects.id = pledges.project_id
  GROUP BY projects.title
  HAVING amount_over >= 0;"

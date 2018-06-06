814b45642f938e5d69e1a66c5d4ff0ed4ed35f6b
Trac Nguyen <nguyendtrac@gmail.com>
2018-06-06 16:26:05 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -19,7 +19,7 @@ def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_
 "SELECT users.name, users.age, SUM(pledges.amount)
  FROM users
  JOIN pledges
- ON users.id = pledges.project_id
+ ON users.id = pledges.user_id
  GROUP BY users.title
  ORDER BY users.title;"
 end

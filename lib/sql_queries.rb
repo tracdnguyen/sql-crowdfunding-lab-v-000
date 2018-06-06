7b2048005589aca0b38fcf9362b873c82e88f77c
Trac Nguyen <nguyendtrac@gmail.com>
2018-06-06 16:57:52 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -43,7 +43,7 @@ def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_th
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
-"SELECT projects.category, SUM(pledges.amount)
+"SELECT projects.category, cast(pledges.amount AS INTEGER)
  FROM projects
  JOIN pledges
  ON projects.id = pledges.project_id

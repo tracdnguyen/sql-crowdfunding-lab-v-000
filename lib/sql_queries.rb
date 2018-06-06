a2fbe3640943bd16bfd0429b80794a1ee96dfe28
Trac Nguyen <nguyendtrac@gmail.com>
2018-06-06 16:58:03 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -45,7 +45,7 @@ end
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
 "SELECT projects.category, cast(pledges.amount AS INTEGER)
  FROM projects
- JOIN pledges
+ LEFT JOIN pledges
  ON projects.id = pledges.project_id
  WHERE projects.category = 'music';"
 end

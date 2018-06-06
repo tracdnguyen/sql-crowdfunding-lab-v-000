61b70ec9cc485a3a15db6c7decf6ad9f72f4984a
Trac Nguyen <nguyendtrac@gmail.com>
2018-06-06 16:33:37 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -30,7 +30,7 @@ def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_
  JOIN pledges
  ON projects.id = pledges.project_id
  GROUP BY projects.title
- HAVING amount_over >= 0;"
+ HAVING amount_over > 0;"
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name

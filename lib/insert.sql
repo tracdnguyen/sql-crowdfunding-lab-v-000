9103e5cefedae8eb75ca650c02a10fc07316efc8
Trac Nguyen <nguyendtrac@gmail.com>
2018-06-06 16:17:48 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,13 +1,14 @@
-INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES (1, 'Help me buy a guitar', 'music', 500.00, '2013-06-30', '2013-07-30');
-INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES (2, 'My book on SQL', 'books', 20.00, '2013-01-01', '2014-01-01');
-INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES (3, 'The next Harry Potter', 'books', 1000.00, '2013-05-30', '2015-05-30');
-INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES (4, 'Animal shelter needs dog food', 'charity', 400.00, '2013-03-14', '2013-06-30');
-INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES (5, 'Voldement needs a body', 'charity', 6000.00, '2013-03-20', '2013-09-20');
-INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES (6, 'The next Inna-Gadda-Davida', 'music', 200.00, '2014-12-30', '2015-12-30');
-INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES (7, 'Iguana needs tail operation', 'charity', 2000.00, '2013-10-02', '2013-10-30');
-INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES (8, 'I have bed bugs!', 'charity', 800.00, '2014-06-30', '2014-06-31');
-INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES (9, 'I want to teach English in China', 'charity', 3000.00, '2013-06-30', '2013-09-30');
-INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES (10, 'Help save birds of paradise', 'charity', 5000.00, '2012-03-20', '2013-06-30');
+INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES
+(1, 'Help me buy a guitar', 'music', 500.00, '2013-06-30', '2013-07-30'),
+(2, 'My book on SQL', 'books', 20.00, '2013-01-01', '2014-01-01'),
+(3, 'The next Harry Potter', 'books', 1000.00, '2013-05-30', '2015-05-30'),
+(4, 'Animal shelter needs dog food', 'charity', 400.00, '2013-03-14', '2013-06-30'),
+(5, 'Voldement needs a body', 'charity', 6000.00, '2013-03-20', '2013-09-20'),
+(6, 'The next Inna-Gadda-Davida', 'music', 200.00, '2014-12-30', '2015-12-30'),
+(7, 'Iguana needs tail operation', 'charity', 2000.00, '2013-10-02', '2013-10-30'),
+(8, 'I have bed bugs!', 'charity', 800.00, '2014-06-30', '2014-06-31'),
+(9, 'I want to teach English in China', 'charity', 3000.00, '2013-06-30', '2013-09-30'),
+(10, 'Help save birds of paradise', 'charity', 5000.00, '2012-03-20', '2013-06-30');
 
 INSERT INTO users (id, name, age) VALUES (1, 'Finnebar', 17), (2, 'Bear', 6), (3, 'Iguana', 4), (4, 'Alex', 33),
 (5, 'Amanda', 24), (6, 'Sophie', 24), (7, 'Rosey', 9), (8, 'Victoria', 23), (9, 'Franz', 100), (10, 'Hermione', 30),
